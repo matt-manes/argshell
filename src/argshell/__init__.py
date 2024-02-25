1d220c629569f844fce717744d9476e68218a3b0
matt manes <mattmanes@pm.me>
2024-02-24 18:46:05 -0600

    chore: build v1.6.2

--- src/argshell/__init__.py
+++ src/argshell/__init__.py
@@ -1,4 +1,4 @@
 from .argshell import ArgShell, ArgShellParser, ArgumentParser, Namespace, with_parser
 
-__version__ = "1.6.1"
+__version__ = "1.6.2"
 __all__ = ["ArgShell", "ArgShellParser", "Namespace", "with_parser", "ArgumentParser"]
